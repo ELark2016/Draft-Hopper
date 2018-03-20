32a8bebf28fddf2315411cf893d8b28990962e3f
Topher <tophersymps@gmail.com>
2018-03-20 12:33:48 -0400

    Auto stash before merge of "dev" and "origin/dev"
    Start js file for database tabs

---
+++ assets/js/beerData.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCK8v4mS7joKuDEI03pAmCqQ2CJH77UBFM",
+  authDomain: "project1-drafthopper.firebaseapp.com",
+  databaseURL: "https://project1-drafthopper.firebaseio.com",
+  projectId: "project1-drafthopper",
+  storageBucket: "project1-drafthopper.appspot.com",
+  messagingSenderId: "1018212755473"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
